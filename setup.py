9cb7148193e7bddc072904f699aa5f46be4aecd5
Johanderson <johander1822@gmail.com>
2021-07-16 13:28:02 +0200

    fix for pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 """Setup module."""
 
 from setuptools import setup
-from setuptools import find_packages
 from pkg_resources import parse_requirements
 
 
@@ -20,10 +19,23 @@ setup(
     name='onecache',
     version='0.0.1',
     description='Python cache for sync and async code',
+    long_description=read_file("README.md"),
+    long_description_content_type="text/markdown",
     author='Johanderson Mogollon',
     author_email='johander1822@gmail.com',
     license='MIT',
-    packages=find_packages(),
+    packages=['onecache'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
     install_requires=requirements('./requirements.txt'),
     extras_require={
         'test': requirements('./test-requirements.txt')
